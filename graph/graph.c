5602857beb5c778e59a859ce60ecaaaa67118847
LiuJia <1550990611@qq.com>
2014-12-13 22:57:45 +0800

    增加邻接表存储

--- graph/graph.c
+++ graph/graph.c
@@ -1,4 +1,4 @@
-/*图的临接矩阵存储*/
+/*图的邻接矩阵存储*/
 typedef char VertexType;
 typedef int  EdgeType;
 #define MAXVEX 100
@@ -35,6 +35,58 @@ void CreateGraph(MGraph *G)
 		scanf("%d,%d,%d",&i,&j,&weight);
 		G->arc[i][j]=weight;
 		G->arc[j][i]=G->arc[i][j];
+	}
+}
+
+/*图的邻接表存储*/
+typedef char VertexType;
+typedef int  EdgeType;
+
+typedef struct EdgeNode
+{
+	int adjvex;
+	EdgeType weight;
+	struct EdgeNode *next;
+} EdgeNode;
+
+typedef struct VertexNode
+{
+	VertexType data;
+	EdgeNode *firstedge;
+}VertexNode,AdjList[MAXVEX];
+
+typedef struct 
+{
+	AdjList adjList;
+	int numVertexes,numEdges;
+}GraphAdjList; 
+
+void CreateALGraph(GraphAdjList *G)
+{
+	int i,j,k;
+	EdgeNode *e;
+	printf("输入顶点数和边数：\n")；
+	scanf("%d,%d",&G->numVertexes,&G->numEdges);
+	for(i=0;i<G->numVertexes;i++)
+	{
+		scanf(&G->adjList[i].data);
+		G->adjList[i].firstedge = NULL;
+	} 
+	for(k=0;k<G->numEdges;k++)
+	{
+		printf("输入边(vi,vj)上的顶点序号：\n");
+		scanf("%d,%d",&i,&j);
+		e = (EdgeNode*)malloc(sizeof(EdgeNode));
+		
+		e->adjvex=j;
+		e->next = G->adjList[i].firstedge;
+		G->adjList[i].firstedge = e;
+		
+		e = (EdgeNode*)malloc(sizeof(EdgeNode));
+		
+		e->adjvex=i;
+		e->next = G->adjList[j].firstedge;
+		G->adjList[j].firstedge = e; 
 	}
 }
 
